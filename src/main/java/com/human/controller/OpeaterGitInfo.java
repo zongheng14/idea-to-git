e9f5290b5ac96a25396fa457b7db4f4f4524c13e
zongheng14 <zongheng1314@163.com>
2021-12-12 17:28:28 +0800

    获取git配置信息

---
+++ src/main/java/com/human/controller/OpeaterGitInfo.java
@@ -0,0 +1,14 @@
+package com.human.controller;
+
+/**
+ * @Author NineOne
+ * @Site
+ * @Company
+ * @Create by 2021-12-12 17:26
+ **/
+public class OpeaterGitInfo {
+
+    private String getGitInfo(){
+        System.out.println("~~~……………………");
+    }
+}
